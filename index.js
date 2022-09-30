c242abcdb6d33da48f92351e9f7941e31f35b9c9
Carolina <carolinaruo@gmail.com>
2022-09-30 11:02:31 -0300

    Teste chalk lib

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
+import chalk from 'chalk';
+
+console.log(chalk.blue('hello world'));
+
 console.log('olá mundo');
+
+console.log(`
+CPU: ${chalk.red('90%')}
+RAM: ${chalk.green('40%')}
+DISK: ${chalk.yellow('70%')}
+`);
